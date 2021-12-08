7ad1f2ccf7903af96198014c8727f228e3cd7f2f
Balakrishnan <balakrishnan@linumiz.com>
2021-12-08 17:00:28 +0530

    celcius to faherenheit

---
+++ exe1.4.c
@@ -0,0 +1,26 @@
+/* to print celcius to faherenheit table*/
+
+
+#include <stdio.h>
+
+int main()
+{
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  
+  printf("To print celcius to faherenheit table\n");
+  celsius = lower;
+  printf("celcius\tfaherenheit\n");
+  while(celsius <= upper)
+  {
+    fahr = (9.0/5.0) * celsius + 32.0;
+    printf("%3.0f\t-%6.1f\n", celsius, fahr);
+    celsius = celsius + step;
+}
+  return 0;
+}

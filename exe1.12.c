88ed0ba78ea55b96cdc1f324be116df5b591f087
Balakrishnan <balakrishnan@linumiz.com>
2021-12-08 17:16:05 +0530

    one word per line

---
+++ exe1.12.c
@@ -0,0 +1,26 @@
+/*one word per line*/
+#include<stdio.h>
+#define IN 1
+#define OUT 0
+int main()
+{
+int c,state;
+//state=OUT;
+while((c=getchar())!=EOF)
+{
+if(c==' '||c=='\t')
+state=OUT;
+else if(state==OUT){
+state=IN;
+printf("\n");
+putchar(c);
+}
+else
+{
+putchar(c);
+}
+}
+return 0;
+}
+
+
